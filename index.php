1f4b37e21f61a7a2a15524bf9ed8e29ccade0175
SarahAbdeldaym <97462340+SarahAbdeldaym@users.noreply.github.com>
2022-03-10 17:37:53 +0200

    adding database class and config file with index class

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$DB=new Database();
+
+// $table=Database::table("users")->get();
+// $users = Database::getAllUsers();
+
+// print_r($users);
+
+require_once("views/login.php");
+
+?>

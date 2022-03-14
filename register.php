0f4191f06a951fec6f4daa0f0369e84e26ab9641
SarahAbdeldaym <97462340+SarahAbdeldaym@users.noreply.github.com>
2022-03-14 16:19:23 +0200

    correct regex and validation after testting

--- register.php
+++ register.php
@@ -1,6 +1,9 @@
 <?php
-require_once("config.php");
+
+require_once("vendor/autoload.php");
+
 $register_validation_errors_arr = [];
+$register_user = new Users;
 
 if (isset($_POST["submit"])) {
 
@@ -12,11 +15,11 @@ if (isset($_POST["submit"])) {
         || empty($_POST["cvv"]) || empty($_POST["passwordConfirm"])
         ||  empty($_POST["expirationDate"])
     ) {
-        array_push($register_validation_errors_arr, "all input must be filled");
+        array_push($register_validation_errors_arr, "All input must be filled.");
         $validated = false;
     } else {
 
-        if (!(strlen(trim($_POST["username"])) < _max_username_length_)) {
+        if (!(strlen(trim($POST["username"])) < _max_username_length)) {
             array_push($register_validation_errors_arr, "Name is not valid!");
             $validated = false;
         }
@@ -26,32 +29,47 @@ if (isset($_POST["submit"])) {
             $validated = false;
         }
 
-        if (isset($_POST["password"]) && isset($_POST["passwordConfirm"])) {
-            $uppercase = preg_match('@[A-Z]@', $_POST["password"]);
-            $lowercase = preg_match('@[a-z]@', $_POST["password"]);
-            $number    = preg_match('@[0-9]@', $_POST["password"]);
-            if ($_POST["password"] == $_POST["passwordConfirm"]) {
-                if (!(strlen(trim($uppercase || !$lowercase || !$number || strlen($_POST["password"]))) < _max_password_length_)) {
-                    array_push($register_validation_errors_arr, "Password must be a minimum of 8 characters and contain at least one capital letter,
-         a number and no special character such as an underscore or exclamation point.");
-                }
-            } else {
-                array_push($register_validation_errors_arr, "password are not matching dude!!");
-            }
+        if (!(trim(preg_match("/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $_POST["password"])))) {
+            array_push($register_validation_errors_arr, "Password must be Minimum eight characters,
+            at least one uppercase letter, one lowercase letter and one number.");
+            $validated = false;
         }
 
-        if (!(strlen(trim(preg_match('@[0-9]{16}@', $_POST["cardNumber"]))))) {
-            array_push($register_validation_errors_arr, "you have a weird card number !!");
+        if ($_POST["password"] != $_POST["passwordConfirm"]) {
+            array_push($register_validation_errors_arr, "Password are not matching dude!");
+            $validated = false;
+        }
+
+        if (!(trim(preg_match("/^[0-9]{16}$/", $_POST["cardNumber"])))) {
+            array_push($register_validation_errors_arr, "You have a weird card number!");
+            $validated = false;
         }
 
 
-        if (!(strlen(trim(preg_match('@[0-9]{3}@', $_POST["cvv"]))))) {
-            array_push($register_validation_errors_arr, "Please enter a valid CVVg code");
+        if (!(trim(preg_match("/^[0-9]{3}$/", $_POST["cvv"])))) {
+            array_push($register_validation_errors_arr, "CVV in not valid!");
+            $validated = false;
         }
+
 
         if (!preg_match("/^(0[1-9]|1[0-2])[\/][0-9]{2}$/", $_POST["expirationDate"])) {
             array_push($register_validation_errors_arr, "The expire date format is not correct!");
+            $validated = false;
         }
+    }
+
+    // Checking if user is already registered in the database.
+    $register_user = new Users;
+    $email_registered = $register_user->is_registered($_POST["email"]); // True if email is already registered.
+    if ($email_registered) {
+        array_push($register_validation_errors_arr, "Email is already registered with another account, please try with different Email.");
+        $validated = false;
+    }
+
+    // Adding the user to the database
+    if ($validated) {
+        $register_user->register_insert($_POST["username"], $_POST["email"], $_POST["password"]);
+        header('Location: views/login.html');
     }
 }
 
